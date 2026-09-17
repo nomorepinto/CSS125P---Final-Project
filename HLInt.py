6cd99b0f9711cabf2a6b7a1147999a158ec7fec5
Jedrick <bilogdino071@gmail.com>
2026-09-17 13:39:53 +0800

    added full steps to HLInt

--- HLInt.py
+++ HLInt.py
@@ -1,8 +1,12 @@
 import file_writer
+import lexer
+import syntax_checker
 import sys
 
 def main(filename):
     file_writer.write_no_spaces(filename)
+    lexer.lex(filename)
+    syntax_checker.check(filename)
 
 if __name__ == "__main__":
     arg = sys.argv[1]
